8f055b4c10a207ca41ef3c1a84f2a8cd4b94a55b
raunak kumar <raunak@live.com>
2021-06-29 20:40:24 +0530

    Created JOB which ctrates XML file

---
+++ raunak-custom/raunak_custom/cartridge/scripts/changeProductCategory.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var Transaction = require('dw/system/Transaction');
+var File = require('dw/io/File');
+var FileWriter = require('dw/io/FileWriter');
+var XMLStreamWriter = require('dw/io/XMLIndentingStreamWriter');
+
+/**
+ * Crrating XML file In IMPEX folder
+ * @param {products} product
+ * @param {brand} Brandname
+ */
+function createXML(products, brand) {
+    try{
+        var impexFolder = File.IMPEX + File.SEPARATOR + "src/catalog/";
+        var impexFolderCheck = new File(impexFolder);
+        if (!impexFolderCheck.exists()) {
+            impexFolderCheck.mkdirs();
+        }
+
+        // create export XML file for products
+        // create new fileName for export XML file
+        var fileNameProcessed = impexFolder + brand + '.xml';
+        var targetFileProcessed = new File(fileNameProcessed);
+
+        if (targetFileProcessed.exists()) {
+            targetFileProcessed.remove();
+        }
+        targetFileProcessed.createNewFile();
+
+        var fileWriter = new FileWriter(targetFileProcessed, "UTF-8");
+        var xsw = new XMLStreamWriter(fileWriter);
+        xsw.writeStartDocument();
+            xsw.writeStartElement("catalog");
+                xsw.writeAttribute("xmlns", "http://www.demandware.com/xml/impex/catalog/2006-10-31");
+                xsw.writeAttribute("catalog-id", "apparel-m-catalog");
+                for(var i=0; i<products.length; i++) {
+                    xsw.writeStartElement("product");
+                        xsw.writeAttribute("product-id", products[i].ID);
+                        xsw.writeStartElement("online-flag");
+                            xsw.writeCharacters(products[i].online);
+                        xsw.writeEndElement();
+                        xsw.writeStartElement("available-flag");
+                            xsw.writeCharacters(products[i].availabilityModel.isOrderable());
+                        xsw.writeEndElement();
+                        xsw.writeStartElement("brand");
+                            xsw.writeCharacters(products[i].brand);
+                        xsw.writeEndElement();
+                    xsw.writeEndElement();
+                    xsw.writeStartElement("category-assignment");
+                        xsw.writeAttribute("category-id", "womens-accessories-scarves");
+                        xsw.writeAttribute("product-id", products[i].ID);
+                        xsw.writeStartElement("primary-flag");
+                            xsw.writeCharacters(true);
+                        xsw.writeEndElement();
+                    xsw.writeEndElement();
+                }
+            xsw.writeEndElement();
+        xsw.writeEndDocument();
+        xsw.close();
+        fileWriter.close();
+    } catch (exception) {
+        log.error("changeProductCategory.js : There was an error creating the xml file: {0}", exception.message);
+    }
+}
+
+module.exports = {
+
+    /**
+* This function getting called when JOB runs
+ * Crrating XML file In IMPEX folder
+ * @param {params} Job Params
+ */
+generateXML: function generateXML(params) {
+    var Iterator = require('dw/util/Iterator');
+    params;
+    var productMgr = require('dw/catalog/ProductMgr');
+    var products = productMgr.queryAllSiteProducts();
+    var listProducts = products.asList();
+    var result = listProducts.toArray().filter(function(product) {
+    return product.brand === params.brandName;
+    });
+    createXML(result, params.brandName);
+    products.close();
+}
+}
